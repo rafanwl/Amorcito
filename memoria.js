1dfe09ee704e79d12c5268b974c38acb40160a19
rafanwl <128259148+rafanwl@users.noreply.github.com>
2026-01-07 22:41:20 -0500

    Add files via upload

---
+++ memoria.js
@@ -0,0 +1,69 @@
+const images = [
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/6.jpg"
+];
+
+// duplicamos para hacer parejas
+let cards = [...images, ...images];
+cards.sort(() => 0.5 - Math.random());
+
+const board = document.getElementById("gameBoard");
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matched = 0;
+
+cards.forEach(img => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <div class="card-inner">
+      <div class="card-front"></div>
+      <div class="card-back" style="background-image:url('${img}')"></div>
+    </div>
+  `;
+
+  card.addEventListener("click", () => flipCard(card, img));
+  board.appendChild(card);
+});
+
+function flipCard(card, img) {
+  if (lockBoard || card === firstCard) return;
+
+  card.classList.add("flip");
+
+  if (!firstCard) {
+    firstCard = card;
+    firstCard.dataset.img = img;
+    return;
+  }
+
+  secondCard = card;
+  secondCard.dataset.img = img;
+  lockBoard = true;
+
+  if (firstCard.dataset.img === secondCard.dataset.img) {
+    matched += 2;
+    resetTurn();
+
+    if (matched === cards.length) {
+      document.getElementById("winMessage").classList.remove("hidden");
+    }
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      resetTurn();
+    }, 800);
+  }
+}
+
+function resetTurn() {
+  [firstCard, secondCard] = [null, null];
+  lockBoard = false;
+}
